f76ccceca6a866e8ce0f314cf5d4c8ff74dbfc97
kunal9038 <ks635634@gmail.com>
2026-03-23 13:29:04 +0530

    test auto deploy

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def login():
     return """
-    <h1 style='color:green;'>🚀 CI/CD Pipeline Working!</h1>
-    <h2>DevOps Deployment Success ✅</h2>
-    <p>Version 2 - Auto Deploy Test</p>
+    AUTO DEPLOY TEST 🔥
     """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
