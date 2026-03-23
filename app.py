028f8918ebb9828300fb7b111eadfffc20967a51
kunal9038 <ks635634@gmail.com>
2026-03-23 13:40:34 +0530

    final automation test

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return """
-    AUTO DEPLOY TEST 🔥
-    """
+    return "🔥 FULLY AUTOMATED DEPLOYMENT SUCCESS 🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
